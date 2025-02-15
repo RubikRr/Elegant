97cfe6cc94dee52f4f72d8580167cf614f1aa451
vova_chakalov <vova.chakalov.2003@gmail.com>
2025-02-15 23:57:25 +0300

    add migration

--- Elegant.DAL/Migrations/20250215205656_ChangeUserIdTypeInCart.Designer.cs
+++ Elegant.DAL/Migrations/20250215205656_ChangeUserIdTypeInCart.Designer.cs
@@ -4,6 +4,7 @@ using Elegant.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Elegant.DAL.Migrations
 {
     [DbContext(typeof(EfCoreDbContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250215205656_ChangeUserIdTypeInCart")]
+    partial class ChangeUserIdTypeInCart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
